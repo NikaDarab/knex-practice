dccd3548e02cd6c84870b209d307e44620388344
Nika Darab <nikadarab@pm.me>
2021-01-09 19:23:07 -0600

    blogful seeds and table

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,24 @@
+INSERT into blogful_articles (title, content, date_published)
+VALUES 
+('title1', 'laudantium rerum sed',  now() ),
+('title2', 'laudantium rerum sed',  now() ),
+('title3', 'laudantium rerum sed',  now() ),
+('title4', 'laudantium rerum sed',  now() - '21 days'::INTERVAL),
+('title5', 'laudantium rerum sed',  now() - '21 days'::INTERVAL),
+('title6', 'laudantium rerum sed',  now() - '21 days'::INTERVAL),  
+('title7', 'laudantium rerum sed',  now() - '21 days'::INTERVAL),  
+('title8', 'laudantium rerum sed',  now() - '21 days'::INTERVAL),  
+('title9', 'laudantium rerum sed',  now() - '21 days'::INTERVAL), 
+('title10', 'laudantium rerum sed',  now() - '21 days'::INTERVAL),  
+('title11', 'laudantium rerum sed',  now() - '21 days'::INTERVAL),   
+('title12', 'laudantium rerum sed',  now() - '21 days'::INTERVAL), 
+('title13', 'laudantium rerum sed',  now() - '21 days'::INTERVAL),  
+('title14', 'laudantium rerum sed',  now() - '21 days'::INTERVAL), 
+('title15', 'laudantium rerum sed',  now() - '21 days'::INTERVAL),  
+('title16', 'laudantium rerum sed',  now() - '21 days'::INTERVAL) , 
+('title17', 'laudantium rerum sed',  now() - '21 days'::INTERVAL) , 
+('title18', 'laudantium rerum sed',  now() - '21 days'::INTERVAL) , 
+('title19', 'laudantium rerum sed',  now() ) , 
+('title20', 'laudantium rerum sed',  now())
+;
+ 
