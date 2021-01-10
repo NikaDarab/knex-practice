2f7d97006a7ecfff4fe51746b8bfacd66cccb11e
Nika Darab <nikadarab@pm.me>
2021-01-09 18:43:02 -0600

    itemsAddedAfter

--- src/drills.js
+++ src/drills.js
@@ -18,11 +18,31 @@ const paginateItems = (pageNumber) => {
   const itemsPerPage = 6;
   const offset = itemsPerPage * (pageNumber - 1);
   knexInstance
-    .select("shopping_list")
+    .select("name")
     .from("shopping_list")
     .limit(itemsPerPage)
     .offset(offset)
     .then((result) => console.log(result));
 };
+
+const itemsAddedAfter = (daysAgo) => {
+  knexInstance
+    .select("name", "price")
+
+    .count("date_added AS days")
+    .where(
+      "date_added",
+      ">",
+      knexInstance.raw("now()-'?? days'::INTERVAL", daysAgo)
+    )
+    .from("shopping_list")
+    .groupBy("name", "price")
+    .orderBy([
+      { column: "name", order: "ASC" },
+      { column: "days", order: "DESC" },
+    ])
+    .then((result) => console.log(result));
+};
 // itemsWithText("fish");
-paginateItems(2);
+// paginateItems(2);
+itemsAddedAfter(21);
