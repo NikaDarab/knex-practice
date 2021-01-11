7c2fe7ae9056a0a5f313a88450a9fa000d82b668
Nika Darab <nikadarab@pm.me>
2021-01-10 19:48:11 -0600

    testItems

--- test/shopping-list-service.spec.js
+++ test/shopping-list-service.spec.js
@@ -3,14 +3,36 @@ const knex = require("knex");
 
 describe(`Articles service object`, function () {
   let db;
-
+  let testItems = [
+    {
+      name: "test name1",
+      price: 12.23,
+      category: "Main",
+      checked: true,
+    },
+    {
+      name: "test name1",
+      price: 22.23,
+      category: "Main",
+      checked: true,
+    },
+    {
+      name: "test name3",
+      price: 32.23,
+      category: "Main",
+      checked: true,
+    },
+  ];
   before(() => {
     db = knex({
       client: "pg",
       connection: process.env.TEST_DB_URL,
     });
   });
-
+  before(() => {
+    return db.into("shopping_list").insert(testItems);
+  });
+  after(() => db.destroy());
   describe("Shopping-list service object", () => {
     describe("getAllItems()", () => {
       it("resolves all items from 'shopping_list table", () => {});
