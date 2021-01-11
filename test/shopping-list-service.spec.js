c4659fd5002ffddcc587d905041d4170b5dcfb9e
Nika Darab <nikadarab@pm.me>
2021-01-10 19:31:30 -0600

    knex instance

--- test/shopping-list-service.spec.js
+++ test/shopping-list-service.spec.js
@@ -1,5 +1,19 @@
-describe(`Shopping-list service object`, () => {
-  it(`Should run the tests`, () => {
-    expect(true).to.eql(false);
+const ShoppingService = require("../src/shopping-list-service");
+const knex = require("knex");
+
+describe(`Articles service object`, function () {
+  let db;
+
+  before(() => {
+    db = knex({
+      client: "pg",
+      connection: process.env.TEST_DB_URL,
+    });
+  });
+
+  describe("Shopping-list service object", () => {
+    describe("getAllItems()", () => {
+      it("resolves all items from 'shopping_list table", () => {});
+    });
   });
 });
