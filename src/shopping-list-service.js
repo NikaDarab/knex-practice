a933c9f4e5568b65af62ff8fa42c46d70712174f
Nika Darab <nikadarab@pm.me>
2021-01-10 22:36:22 -0600

    deleteItem

--- src/shopping-list-service.js
+++ src/shopping-list-service.js
@@ -15,5 +15,8 @@ const ShoppingService = {
   getById(knex, id) {
     return knex.from("shopping_list").select("*").where("id", id).first();
   },
+  deleteItem(knex, id) {
+    return knex("shopping_list").where({ id }).delete();
+  },
 };
 module.exports = ShoppingService;
--- test/shopping-list-service.spec.js
+++ test/shopping-list-service.spec.js
@@ -1,6 +1,7 @@
 const ShoppingService = require("../src/shopping-list-service");
 const knex = require("knex");
 const { expect } = require("chai");
+const { insertItems } = require("../src/shopping-list-service");
 
 describe("shopping service object", function () {
   let db;
@@ -10,7 +11,7 @@ describe("shopping service object", function () {
       name: "test name1",
       price: "12.23",
       category: "Main",
-      checked: true,
+      checked: false,
       date_added: new Date("2029-01-22T16:28:32.615Z"),
     },
     {
@@ -18,7 +19,7 @@ describe("shopping service object", function () {
       name: "test name1",
       price: "22.23",
       category: "Main",
-      checked: true,
+      checked: false,
       date_added: new Date("2029-01-22T16:28:32.615Z"),
     },
     {
@@ -26,7 +27,7 @@ describe("shopping service object", function () {
       name: "test name3",
       price: "32.23",
       category: "Main",
-      checked: true,
+      checked: false,
       date_added: new Date("2029-01-22T16:28:32.615Z"),
     },
   ];
@@ -43,30 +44,39 @@ describe("shopping service object", function () {
     beforeEach(() => {
       return db.into("shopping_list").insert(testItems);
     });
-  });
 
-  it("getAllItems() resolves all items from 'shopping_list table", () => {
-    return ShoppingService.getAllItems(db).then((actual) => {
-      expect(actual).to.eql(
-        testItems.map((item) => ({
-          ...item,
-          checked: false,
-        }))
-      );
-    });
-  });
-  it(`getById() resolved an item from 'shopping_list' table`, () => {
-    const thirdId = 3;
-    const thirdTestItem = testItems[thirdId - 1];
-    return ShoppingService.getById(db, thirdId).then((actual) => {
-      expect(actual).to.eql({
-        id: thirdId,
-        name: thirdTestItem.name,
-        price: thirdTestItem.price,
-        category: thirdTestItem.category,
-        checked: false,
-        date_added: thirdTestItem.date_added,
+    it("getAllItems() resolves all items from 'shopping_list table", () => {
+      return ShoppingService.getAllItems(db).then((actual) => {
+        expect(actual).to.eql(
+          testItems.map((item) => ({
+            ...item,
+            checked: false,
+          }))
+        );
       });
+    });
+    it(`getById() resolved an item from 'shopping_list' table`, () => {
+      const thirdId = 3;
+      const thirdTestItem = testItems[thirdId - 1];
+      return ShoppingService.getById(db, thirdId).then((actual) => {
+        expect(actual).to.eql({
+          id: thirdId,
+          name: thirdTestItem.name,
+          price: thirdTestItem.price,
+          category: thirdTestItem.category,
+          checked: false,
+          date_added: thirdTestItem.date_added,
+        });
+      });
+    });
+    it("deleteItem() removes an item by id from shopping_list table", () => {
+      const itemId = 3;
+      return ShoppingService.deleteItem(db, itemId)
+        .then(() => ShoppingService.getAllItems(db))
+        .then((allItems) => {
+          const expected = testItems.filter((item) => item.id !== itemId);
+          expect(allItems).to.eql(expected);
+        });
     });
   });
   context("Given 'shopping_list' has no data", () => {
@@ -80,7 +90,7 @@ describe("shopping service object", function () {
         name: "Test new name",
         price: "20.00",
         category: "Main",
-        checked: true,
+        checked: false,
         date_added: new Date("2020-01-01T00:00:00.000Z"),
       };
       return ShoppingService.insertItems(db, newItem).then((actual) => {
